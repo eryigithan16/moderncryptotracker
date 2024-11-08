a441cb8b7455754591a91dfa04b2b797187fe0f7
eryigithan16 <ygthnerygt1615@outlook.com>
2024-11-08 10:14:01 +0300

    adaptive navigation pane completed

--- app/src/main/java/com/yeryigit/moderncryptotracker/MainActivity.kt
+++ app/src/main/java/com/yeryigit/moderncryptotracker/MainActivity.kt
@@ -1,24 +1,15 @@
 package com.yeryigit.moderncryptotracker
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.yeryigit.moderncryptotracker.core.presentation.util.ObserveAsEvents
-import com.yeryigit.moderncryptotracker.core.presentation.util.toString
-import com.yeryigit.moderncryptotracker.crypto.presentation.coin_list.CoinListEvent
-import com.yeryigit.moderncryptotracker.crypto.presentation.coin_list.CoinListScreen
-import com.yeryigit.moderncryptotracker.crypto.presentation.coin_list.CoinListViewModel
+import com.yeryigit.moderncryptotracker.core.navigation.AdaptiveCoinListDetailPane
 import com.yeryigit.moderncryptotracker.ui.theme.ModernCryptoTrackerTheme
-import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,22 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ModernCryptoTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val viewModel = koinViewModel<CoinListViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-                    val context = LocalContext.current
-                    ObserveAsEvents(events = viewModel.events) { event ->
-                        when (event) {
-                            is CoinListEvent.Error -> {
-                                Toast.makeText(
-                                    context,
-                                    event.error.toString(context),
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
-                    }
-                    CoinListScreen(
-                        state = state,
+                    AdaptiveCoinListDetailPane(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/yeryigit/moderncryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/yeryigit/moderncryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -1,6 +1,6 @@
 package com.yeryigit.moderncryptotracker.crypto.presentation.coin_list
 
-import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,46 +9,63 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.repeatOnLifecycle
-import com.yeryigit.moderncryptotracker.core.presentation.util.toString
 import com.yeryigit.moderncryptotracker.crypto.presentation.coin_list.components.CoinListItem
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
+import com.yeryigit.moderncryptotracker.crypto.presentation.coin_list.components.previewCoin
+import com.yeryigit.moderncryptotracker.ui.theme.ModernCryptoTrackerTheme
 
 @Composable
 fun CoinListScreen(
     state: CoinListState,
+    onAction: (CoinListAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
     if (state.isLoading) {
         Box(
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             CircularProgressIndicator()
         }
     } else {
         LazyColumn(
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier
+                .fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(state.coins) { coinUi ->
                 CoinListItem(
                     coinUi = coinUi,
-                    onClick = { /*TODO*/ },
+                    onClick = {
+                        onAction(CoinListAction.OnCoinClick(coinUi))
+                    },
                     modifier = Modifier.fillMaxWidth()
                 )
                 HorizontalDivider()
             }
         }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinListScreenPreview() {
+    ModernCryptoTrackerTheme {
+        CoinListScreen(
+            state = CoinListState(
+                coins = (1..100).map {
+                    previewCoin.copy(id = it.toString())
+                }
+            ),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background),
+            onAction = {}
+        )
     }
 }
